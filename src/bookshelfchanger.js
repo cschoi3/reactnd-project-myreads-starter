586c6e2c158943b18f7c98f185454b8679acf47d
CC <cschoi3@gmail.com>
2018-07-02 18:17:33 -0700

    feat: add book on selecting BookShelfChanger
    added onClick alongside onChange to select in BookShelfChanger to account for situations where user adds book to the default/currently selected book shelf

--- src/bookshelfchanger.js
+++ src/bookshelfchanger.js
@@ -22,7 +22,7 @@ class BookShelfChanger extends Component {
 	render() {
 		
 		return (<div className="book-shelf-changer">
-			<select value={this.state.value} onChange={this.handleChange}>
+			<select value={this.state.value} onChange={this.handleChange} onClick={this.handleChange}>
 				<option value="move" disabled>Move to...</option>
 				<option value="currentlyReading">Currently Reading</option>
 				<option value="wantToRead">Want to Read</option>
